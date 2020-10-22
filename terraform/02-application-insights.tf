46b36061fbf64908c4d3dfb102e5371496bdd350
James Ha <62304682+James-LeHa@users.noreply.github.com>
2020-10-22 15:49:52 -0400

    Update 02-application-insights.tf

--- terraform/02-application-insights.tf
+++ terraform/02-application-insights.tf
@@ -3,7 +3,7 @@ resource "azurerm_app_service_plan" "main" {
   resource_group_name = "${azurerm_resource_group.main.name}"
   location            = "${azurerm_resource_group.main.location}"
 
-    sku {
+  sku {
     tier = "Standard"
     size = "S1"
   }
