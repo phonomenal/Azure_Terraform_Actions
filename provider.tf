c7590218cec0242eb5ecd4ee08f24fa5f41ed824
James Ha <james-leha@github.com>
2020-10-22 19:34:30 -0400

    use working directory

--- provider.tf
+++
@@ -1,9 +0,0 @@
-provider "azurerm" {
-  version = "=2.20.0"
-  features {}
-
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
-  client_id       = var.agent_client_id
-  client_secret   = var.agent_client_secret
-}
