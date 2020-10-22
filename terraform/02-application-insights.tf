6719b777174efd71983e624a19a1df9e79f73647
James Ha <62304682+James-LeHa@users.noreply.github.com>
2020-10-22 15:53:05 -0400

    Update 02-application-insights.tf

--- terraform/02-application-insights.tf
+++ terraform/02-application-insights.tf
@@ -11,9 +11,9 @@ resource "azurerm_app_service_plan" "main" {
 
 resource "azurerm_app_service" "main" {
   name                = "example-app-service"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_app_service_plan.example.id
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  app_service_plan_id = azurerm_app_service_plan.main.id
 
   site_config {
     dotnet_framework_version = "v4.0"
