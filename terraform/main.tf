b70474c8ae9facb69ee7e07da52c850672d64e9b
James Ha <james-leha@github.com>
2020-10-27 17:31:53 -0400

    remove clientid tf variable test

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ provider "azurerm" {
   tenant_id       = var.tenant_id
   subscription_id = var.subscription_id
   #client_id       = var.agent_client_id
-  client_secret   = var.agent_client_secret
+  client_secret = var.agent_client_secret
 }
 
 variable "agent_client_id" {}
