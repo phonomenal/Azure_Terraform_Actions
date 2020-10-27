17166f0ad9e7c0934f69a125117ba92634244d08
James Ha <james-leha@github.com>
2020-10-27 13:26:53 -0400

    assing single policy test

--- terraform/01-resource-group.tf
+++ terraform/01-resource-group.tf
@@ -14,6 +14,7 @@ resource "azurerm_policy_assignment" "HITRUST_HIPPA" {
   name                 = "Diagnostic logs in App Services should be enabled"
   scope                = azurerm_resource_group.main.id
   policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/b607c5de-e7d9-4eee-9e5c-83f1bcee4fa0"
+}
 
 #   metadata = <<METADATA
 #     {
@@ -43,4 +44,4 @@ resource "azurerm_policy_assignment" "HITRUST_HIPPA" {
 # }
 # PARAMETERS
 
-}
+# }
