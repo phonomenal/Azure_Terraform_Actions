19cf00a3bb8414dac7dc92a1f0f6eb7b521e845e
James Ha <james-leha@github.com>
2020-10-27 16:32:49 -0400

    tf variable updates fmt

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -6,14 +6,14 @@ variable "subscription_id" {}
 variable "tenant_id" {}
 
 variable "app_service_name" {
-  type          = string
-  description   = "The requested name for the Azure App Service, must be unique"
+  type        = string
+  description = "The requested name for the Azure App Service, must be unique"
 }
 
 variable "location" {
-  type          = string
-  default       = "eastus"
-  description   = "The Azure region location for the Resource Group where instances will be created"
+  type        = string
+  default     = "eastus"
+  description = "The Azure region location for the Resource Group where instances will be created"
 
   validation {
     condition     = contains(["eastus", "eastus2", "centralUS", "northcentralus", "southcentralus"], var.location)
@@ -22,21 +22,21 @@ variable "location" {
 }
 
 variable "requesting_team" {
-  type          = string
-  description   = "The name of the team requesting these Azure resources"
+  type        = string
+  description = "The name of the team requesting these Azure resources"
 
 }
 
 variable "sku_tier" {
-  type          = string
-  default       = "Standard"
-  description   = "The Azure SKU Tier for the requested Resource"
+  type        = string
+  default     = "Standard"
+  description = "The Azure SKU Tier for the requested Resource"
 
 }
 variable "sku_size" {
-  type          = string
-  default       = "S1"
-  description   = "The Azure SKU Size for the requested Resource"
+  type        = string
+  default     = "S1"
+  description = "The Azure SKU Size for the requested Resource"
 
 }
 
