2f5bba6e8aee17eb6a4e92eb38e83935fcdfff94
James Ha <james-leha@github.com>
2020-10-27 13:23:45 -0400

    assing single policy test

--- terraform/01-resource-group.tf
+++ terraform/01-resource-group.tf
@@ -11,36 +11,36 @@ resource "azurerm_resource_group" "main" {
 # }
 
 resource "azurerm_policy_assignment" "HITRUST_HIPPA" {
-  name                 = "HITRUST/HIPAA"
+  name                 = "Diagnostic logs in App Services should be enabled"
   scope                = azurerm_resource_group.main.id
-  policy_definition_id = "/providers/Microsoft.Authorization/policySetDefinitions/a169a624-5599-4385-a696-c8d643089fab"
+  policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/b607c5de-e7d9-4eee-9e5c-83f1bcee4fa0"
 
-  metadata = <<METADATA
-    {
-    "category": "General"
-    }
-METADATA
+#   metadata = <<METADATA
+#     {
+#     "category": "General"
+#     }
+# METADATA
 
-  parameters = <<PARAMETERS
-{
-  "Application names (supports wildcards)": 
-    {
-      "value" :"*" 
-    },
-  "Storage Account Prefix for Regional Storage Account to deploy diagnostic settings for Network Security Groups": 
-    {
-      "value" :"*" 
-    },
-  "Resource Group Name for Storage Account (must exist) to deploy diagnostic settings for Network Security Groups": 
-    {
-      "value" :"*" 
-    },
-  "Certificate thumbprints": 
-    {
-      "value" :"*" 
-    }
+#   parameters = <<PARAMETERS
+# {
+#   "Application names (supports wildcards)": 
+#     {
+#       "value" :"*" 
+#     },
+#   "Storage Account Prefix for Regional Storage Account to deploy diagnostic settings for Network Security Groups": 
+#     {
+#       "value" :"*" 
+#     },
+#   "Resource Group Name for Storage Account (must exist) to deploy diagnostic settings for Network Security Groups": 
+#     {
+#       "value" :"*" 
+#     },
+#   "Certificate thumbprints": 
+#     {
+#       "value" :"*" 
+#     }
   
-}
-PARAMETERS
+# }
+# PARAMETERS
 
 }
